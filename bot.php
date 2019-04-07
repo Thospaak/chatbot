4ad8088938ade3770108278506506af189cba662
Thospaak <thospaak@gmail.com>
2019-04-07 16:02:01 +0700

    edit bot.php

--- bot.php
+++ bot.php
@@ -13,6 +13,25 @@ $request = file_get_contents('php://input');
 // Decode JSON to Array
 $request_array = json_decode($request, true);
 
+if ( sizeof($request_array['events']) > 0 ) {
+    foreach ($request_array['events'] as $event) {
+    
+    $reply_message = '';
+    $reply_token = $event['replyToken'];
+    $data = [
+       'replyToken' => $reply_token,
+       'messages' => [
+          ['type' => 'text', 
+           'text' => json_encode($request_array)]
+       ]
+    ];
+    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+    echo "Result: ".$send_result."\r\n";
+ }
+}
+echo "OK";
+
 function send_reply_message($url, $post_header, $post_body)
 {
     $ch = curl_init($url);
@@ -26,23 +45,4 @@ function send_reply_message($url, $post_header, $post_body)
 
     return $result;
 }
-
-if ( sizeof($request_array['events']) > 0 ) {
-      foreach ($request_array['events'] as $event) {
-      
-      $reply_message = '';
-      $reply_token = $event['replyToken'];
-      $data = [
-         'replyToken' => $reply_token,
-         'messages' => [
-            ['type' => 'text', 
-             'text' => json_encode($request_array)]
-         ]
-      ];
-      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-      $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-      echo "Result: ".$send_result."\r\n";
-   }
-}
-echo "OK";
 ?>
