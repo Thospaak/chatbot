d8734c7e441394a9f9649337179308e3f1675425
Thospaak <thospaak@gmail.com>
2019-04-07 15:47:26 +0700

    reply message

--- bot.php
+++ bot.php
@@ -26,7 +26,7 @@ function send_reply_message($url, $post_header, $post_body)
     return $result;
 }
 
-if ( sizeof($request_array['events']) > 0 ) {
+/*if ( sizeof($request_array['events']) > 0 ) {
       foreach ($request_array['events'] as $event) {
       
       $reply_message = '';
@@ -44,3 +44,20 @@ if ( sizeof($request_array['events']) > 0 ) {
    }
 }
 echo "OK";
+*/
+
+if ( sizeof($request_array['events']) > 0 ) {
+   foreach ($request_array['events'] as $event) {
+      
+      $reply_message = '';
+      $reply_token = $event['replyToken'];
+      $text = $event['message']['text'];
+      $data = [
+         'replyToken' => $reply_token,
+         'messages' => [['type' => 'text', 'text' => $text ]]
+      ];
+      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+      $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
+      echo "Result: ".$send_result."\r\n";
+    }
+}
